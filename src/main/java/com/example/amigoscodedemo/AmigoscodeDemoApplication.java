ab66d31a94378b115479e235321f7687caa3c26e
ShedrackGodson <hedrickgodson23@gmail.com>
2021-09-20 00:35:54 +0300

    first commit

---
+++ src/main/java/com/example/amigoscodedemo/AmigoscodeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.amigoscodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigoscodeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigoscodeDemoApplication.class, args);
+    }
+
+}
